86e181237aca8c5a5c8a88c9f1a0be78f13bf35e
Lisa Hofert <hofertlisa@gmail.com>
2022-05-17 21:11:35 +0200

    progress on acquire, release processing

--- PWR.hs
+++ PWR.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, GeneralisedNewtypeDeriving #-}
 
 module PWR where
 
@@ -11,28 +11,28 @@ import qualified Data.Set as S
 import Trace
 
 ---------- data definitions ----------
-newtype TStamp = TStamp Int  deriving (Ord, Eq, Show)  -- would just using type be good here too?
+
+newtype TStamp = TStamp Int  deriving (Ord, Eq, Show, Num)  -- would just using type be good here too?
 newtype VClock = VClock {clock :: Map Thread TStamp} deriving (Show) -- idea: use intmap, vector, mutable vector?
 data Epoch     = Epoch {id ::  Thread, tstamp :: TStamp}
 data HistEl    = HistEl {epoch :: Epoch, vclock :: VClock}
 
 -- pretty direct copy of global variables for PWR algorithm from paper 
--- missing: "edges", "conc", "evt" are unneeded for simple PWR; "RW" is the result of PWR function
+-- missing: "edges", "conc", "evt" are unneeded for simple PWR
 data PWRGlobal = PWRGlobal { lockS   :: Map Thread (Set Lock),  -- current lockset per thread
-                             vclocks :: Map Thread VClock,      -- current vector clock per thread
+                             vClocks :: Map Thread VClock,      -- current vector clock per thread
                              lastW_C :: Map Var VClock,         -- vector clock of last write on var
                              lastW_T :: Map Var Thread,         -- threads with last write on var
                              lastW_L :: Map Var (Set Lock),     -- lockset of last write on var
                              acq     :: Map Lock Epoch,         -- epoch of last aquire for lock
-                             hist    :: Map Lock [HistEl] }     -- lock history
+                             hist    :: Map Lock [HistEl],      -- lock history
+                             rw      :: Map Var (Set Event) }
 
-
-newtype PWRState = State PWRGlobal
-newtype RW = RW (Map Var (Set Event))  -- result type of computation
 
 ---------- helper functions ----------
 
 -- initialize vector clock for n threads: time stamp 0 for all threads
+-- NOTE: this means we need to pass the number of threads to the algorithm. Is it necessary to work with initialized clocks?
 vInit :: Int -> VClock
 vInit n = VClock $ M.fromList $ map (\i -> (Thread i, TStamp 0)) [0..(n-1)]
 
@@ -41,17 +41,58 @@ vUnion :: VClock -> VClock -> VClock
 vUnion (VClock v) (VClock w) = VClock $ M.unionWith max v w
 
 -- "<" for vector clocks: <= for each time stamp, and not equal (one timestamp must be strictly smaller)
-tBefore :: VClock -> VClock -> Bool
-tBefore (VClock v) (VClock w) = M.isSubmapOfBy (<=) v w && not (M.isSubmapOf v w)
+vBefore :: VClock -> VClock -> Bool
+vBefore (VClock v) (VClock w) = M.isSubmapOfBy (<=) v w && not (M.isSubmapOf v w)
+
+-- increment vector clock for one thread by one
+vInc :: Thread -> VClock -> VClock
+vInc i (VClock v) = VClock $ M.adjust (+ TStamp 1) i v
 
 ---------- PWR ----------
 
-pwr :: [Event] -> RW
+pwr :: [Event] -> Map Var (Set Event)
 pwr = undefined
+
+---------- functions for every type of event ----------
+
+-- modify state for acquire event: 
+-- sync clock -> add lock to lockset of thread -> update last aquire epoch -> increment thread clock
+acquire :: Thread -> Lock -> State PWRGlobal ()
+acquire i y = do
+    s <- get
+    applyW3 i
+    let newLockS = S.union (lockS s M.! i) (S.singleton y)
+    let newAcq = Epoch i (clock (vClocks s M.! i) M.! i)
+    put s {lockS = M.insert i newLockS (lockS s),
+           acq = M.insert y newAcq (acq s)}
+    incClock i
+
+-- modify state for release event:
+-- sync clock -> remove lock from lockset of thread -> update lock history with prev acquire -> increment thread clock
+release :: Thread -> Lock -> State PWRGlobal ()
+release i y = do
+    s <- get
+    applyW3 i
+    let newLockS = S.delete y (lockS s M.! i)
+    let newHist = HistEl (acq s M.! y) (vClocks s M.! i) : (hist s M.! y)
+    put s {lockS = M.insert i newLockS (lockS s),
+           hist = undefined}
+    incClock i
+
+-- modify state for write event:
+-- sync clock -> set concurrent read/writes -> save last write information -> increment thread clock (?)
+write :: Thread -> Lock -> State PWRGlobal ()
+write = undefined
+
+-- modify state for read event:
+-- set concurrent read/writes -> update clock to last write -> sync clock -> set concurrrent read/writes -> increment thread clock (?)
+read :: Thread -> Lock -> State PWRGlobal ()
+read = undefined
 
 ---------- PWR helpers ----------
 
 -- look through lock history. If an acquire in history has earlier timestamp, sync current clock with clock in history
+-- helper function for w3
 syncClock :: VClock -> [HistEl] -> VClock
 syncClock = foldr (\(HistEl (Epoch j k) v') v ->
                     if k < clock v M.! j
@@ -63,24 +104,27 @@ syncClock = foldr (\(HistEl (Epoch j k) v') v ->
 w3 :: VClock -> Set Lock -> Map Lock [HistEl] -> VClock
 w3 v ls h = S.foldr (\l result -> syncClock result (h M.! l)) v ls
 
--- modify state for acquire event
-acquire :: PWRState -> PWRState
-acquire = undefined
 
--- modify state for release event
-release :: PWRState -> PWRState
-release = undefined
+-- can these be made pure...?
+-- apply w3 to vector clock of thread
+applyW3 :: Thread -> State PWRGlobal VClock
+applyW3 i = do
+    s <- get
+    let newClock = w3 (vClocks s M.! i) (lockS s M.! i) (hist s)
+    put s {vClocks = M.insert i newClock (vClocks s)}
+    return newClock
 
--- modify state for write event
-write :: PWRState -> PWRState
-write = undefined
+-- increment vector clock for thread i by one, in thread i
+incClock :: Thread -> State PWRGlobal ()
+incClock i = do
+    s <- get
+    let currClock = vClocks s M.! i
+    put s {vClocks = M.insert i (vInc i currClock) (vClocks s)}
 
--- modify state for read event
-read :: PWRState -> PWRState
-read = undefined
 
 ---------- Tests ----------
 
+-- Didn't think of good values, just put in something to see if it works at all
 syncClockTest :: VClock
 syncClockTest = let v1 = VClock $ M.fromList [(Thread 0, TStamp 0), (Thread 1, TStamp 0)]
                     v2 = VClock $ M.fromList [(Thread 0, TStamp 3), (Thread 1, TStamp 2)]
